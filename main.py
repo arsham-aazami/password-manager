202ae9d35bd98010a3f88d8209abdc5f161cf692
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-12-08 18:32:27 +0330

    adding entries

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-import tkinter
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Password manager")
+window.config(padx=20, pady=20)
 window.minsize(530, 600)
+logo_image = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(row=0, column=1)
 
 
-
+website_entry = Entry(textvariable="Enter website url", width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(textvariable="Enter website url", width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(textvariable="Enter website url", width=21)
+password_entry.grid(row=3, column=1)
+password_generate_button = Button(text="Generate password")
+password_generate_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
