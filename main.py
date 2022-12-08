4bd492fdfe32af347f42991b86907e0ccfba75cb
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-12-08 18:38:13 +0330

    adding labels

--- main.py
+++ main.py
@@ -14,7 +14,17 @@ canvas = Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=logo_image)
 canvas.grid(row=0, column=1)
 
+# Labels
 
+website_label = Label(text="Website:", font=("Arial", 12, "normal"))
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email:", font=("Arial", 12, "normal"))
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:", font=("Arial", 12, "normal"))
+password_label.grid(row=3, column=0)
+
+
+# Entries
 website_entry = Entry(textvariable="Enter website url", width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
 email_entry = Entry(textvariable="Enter website url", width=35)
