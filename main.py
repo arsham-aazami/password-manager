d73d64abf1ac3c83e0989be0cff2c16c4a0ac9f1
Arsham Aazami <arsham20302040aazami@gmail.com>
2022-12-08 17:40:48 +0330

    adding background

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-
-
+import tkinter
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+window = tkinter.Tk()
+window.title("Password manager")
+window.minsize(530, 600)
+
+
+
+
+window.mainloop()
